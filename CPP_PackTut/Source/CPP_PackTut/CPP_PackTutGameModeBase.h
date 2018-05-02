25734c21650bce4cc264fffa169a70e8431f4a70
Daroi\REZA <reza.darooei@gmail.com>
2018-05-02 12:48:07 +0430

    Pack_Intial

---
+++ CPP_PackTut/Source/CPP_PackTut/CPP_PackTutGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_PackTutGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_PACKTUT_API ACPP_PackTutGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
